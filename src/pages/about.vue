d685359e8b58de6fac8e76ec2d571ff9c5b9421f
Joniwus <janluca123@googlemail.com>
2021-11-23 12:52:36 +0100

    Added about text

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,7 +1,14 @@
 <template>
   <navbar></navbar>
   <div class="page">
-    Über uns
+    <h1>Über uns</h1>
+    Mein Mann Ralf, und ich, Gabi, leben im Weserbergland. Unsere Liebe zu den Norwegern begann auf einer Katzenauststellung.
+    Wir haben uns sofort in diese Fellknäule verliebt. Im Oktober 2014 kam dann unsere erste Norwegerin zu uns: Fjellbeks Arizona
+    und so reifte die Idee selber zu züchten. Seit 2015 züchten wir im kleinen Rahmen diese wunderschönen Tiere. Unsere
+    Kitten wuchsin bei uns in der Familie auf, werden an alle Geräusche und Kinder gewöhnt. Wir geben unsere Tiere nicht in
+    Einzelhaltung und ungesicherten Freigang ab.
+
+    Wir sind Mitglied bei Tica Cats
   </div>
 </template>
 <script>
